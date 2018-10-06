58856f3d3f528278076d34529ac6a7229c356f05
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-10-06 13:16:29 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -73,7 +73,7 @@ for i in range(6):
   if i == 2:
     node.addService(pg.Execute(shell="sh", command="sudo su gb773994 -c 'cp /local/repository/source/* /scratch'"))
     node.addService(pg.Execute(shell="sh", command="sudo rm /etc/exports"))
-    node.addService(pg.Execute(shell="sh", command="sudo su gb773994 -c 'cp /local/repository/export_scratch/exports /etc/exports'"))    
+    node.addService(pg.Execute(shell="sh", command="sudo su gb773994 -c 'sudo cp /local/repository/export_scratch/exports /etc/exports'"))    
     
     
   #install mpi on the head node in /software and mount /scratch
@@ -81,11 +81,13 @@ for i in range(6):
     node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
     node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
     node.addService(pg.Execute(shell="sh", command="sudo rm /etc/exports"))
-    node.addService(pg.Execute(shell="sh", command="sudo su gb773994 -c 'cp /local/repository/export_software/exports /etc/exports'"))
+    node.addService(pg.Execute(shell="sh", command="sudo su gb773994 -c 'sudo cp /local/repository/export_software/exports /etc/exports'"))
     node.addService(pg.Execute(shell="sh", command="sudo systemctl restart nfs-server"))
+    node.addService(pg.Execute(shell="sh", command="sleep 10m"))
     node.addService(pg.Execute(shell="sh", command="mount -t nfs 192.168.1.2:/scratch /scratch"))
     
   if i == 3 or i == 4 or i == 5:
+    node.addService(pg.Execute(shell="sh", command="sleep 10m"))
     node.addService(pg.Execute(shell="sh", command="mount -t nfs 192.168.1.1:/software /software"))
     node.addService(pg.Execute(shell="sh", command="mount -t nfs 192.168.1.2:/scratch /scratch"))
     node.addService(pg.Execute(shell="sh", command="sudo -H -u gb773994 bash -c '/local/repository/mpi_path_setup.sh'"))   
