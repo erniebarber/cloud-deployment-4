726623598d8afe3b234d6f2f1c72c44ac3fabb80
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-10-06 13:17:07 -0400

    Update mpi_path_setup.sh

--- mpi_path_setup.sh
+++ mpi_path_setup.sh
@@ -2,3 +2,4 @@
 
 echo "export PATH='$PATH:/software/bin'" >> /users/gb773994/.bashrc
 echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/gb773994/.bashrc
+source /users/gb773994/.bashrc
