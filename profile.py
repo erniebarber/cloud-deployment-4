f845f1e7898b96b0096b2b85658dedb280d64eb5
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-10-06 11:39:11 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -38,13 +38,13 @@ link = request.LAN("lan")
 for i in range(6):
   if i == 0:
     node = request.XenVM("head")
-    node.routable_control_ip = "true"   
+    node.routable_control_ip = "true" 
   elif i == 1:
     node = request.XenVM("metadata")
   elif i == 2:
     node = request.XenVM("storage")
   else:
-    node = request.XenVM("compute" + str(i-2))
+    node = request.XenVM("compute-" + str(i-2))
     node.cores = 2
     node.ram = 4096
     
@@ -55,31 +55,40 @@ for i in range(6):
   iface.addAddress(pg.IPv4Address("192.168.1." + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
   
+  #setup automatic ssh permissions
   node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/passwordless.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
-  
   node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ssh_setup.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo -H -u gb773994 bash -c '/local/repository/ssh_setup.sh'"))
-  
-  node.addService(pg.Execute(shell="sh", command="sudo su gb773994 -c 'cp /local/repository/source/* /users/gb773994'"))
-  
-#  if i == 0 or i == 2:
-#    node.addService(pg.Execute(shell="sh", command="yum -y install nfs-utils nfs-utils-lib"))
-#    node.addService(pg.Execute(shell="sh", command="sudo /etc/init.d/portmap start"))
-#    node.addService(pg.Execute(shell="sh", command="yum -y install portmap"))
-#    node.addService(pg.Execute(shell="sh", command="sudo /etc/init.d/nfs start"))
-#    #node.addService(pg.Execute(shell="sh", command="chkconfig --level 35 portmap on"))
-#    #node.addService(pg.Execute(shell="sh", command="chkconfig --level 35 nfs on"))
-  
-  if i == 0:
+ 
+  #make directories and set permissions
+  if i != 1 and i != 2:
     node.addService(pg.Execute(shell="sh", command="sudo mkdir /software"))
-  if i == 2:
+    node.addService(pg.Execute(shell="sh", command="sudo chmod 777 /software"))
+  if i != 1:
     node.addService(pg.Execute(shell="sh", command="sudo mkdir /scratch"))
-#  
-# if i == 3 or i == 4 or i == 5:
-#    node.addService(pg.Execute(shell="sh", command="mkdir /scratch"))
+    node.addService(pg.Execute(shell="sh", command="sudo chmod 777 /scratch"))
+
+  #setup storage node
+  if i == 1
+    node.addService(pg.Execute(shell="sh", command="sudo su gb773994 -c 'cp /local/repository/source/* /scratch'"))
+    node.addService(pg.Execute(shell="sh", command="sudo rm /etc/exports"))
+    node.addService(pg.Execute(shell="sh", command="sudo su gb773994 -c 'cp /local/repository/export_scratch/exports /etc'"))    
     
+    
+  #install mpi on the head node in /software and mount /scratch
+  if i == 0:
+    node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo rm /etc/exports"))
+    node.addService(pg.Execute(shell="sh", command="sudo su gb773994 -c 'cp /local/repository/export_software/exports /etc'"))
+    node.addService(pg.Execute(shell="sh", command="sudo systemctl restart nfs-server"))
+    node.addService(pg.Execute(shell="sh", command="mount -t nfs 192.168.1.2:/scratch /scratch"))
+    
+  if i == 3 or i == 4 or i == 5:
+    node.addService(pg.Execute(shell="sh", command="mount -t nfs 192.168.1.1:/software /software"))
+    node.addService(pg.Execute(shell="sh", command="mount -t nfs 192.168.1.2:/scratch /scratch"))
+    node.addService(pg.Execute(shell="sh", command="sudo -H -u gb773994 bash -c '/local/repository/mpi_path_setup.sh'"))   
+
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
