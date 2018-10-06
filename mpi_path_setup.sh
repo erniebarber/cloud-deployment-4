709f60c7e19912db5621a910228476e765718151
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-10-06 11:39:00 -0400

    Update mpi_path_setup.sh

--- mpi_path_setup.sh
+++ mpi_path_setup.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/gb773994/.bashrc
 echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/gb773994/.bashrc
