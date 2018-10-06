8e3f951d84655fcce6f675227c792e28bdd217f7
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-10-06 13:09:41 -0400

    Update install_mpi.sh

--- install_mpi.sh
+++ install_mpi.sh
@@ -5,7 +5,7 @@ cd /software
 sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
 sudo tar xzf openmpi-3.1.2.tar.gz
 cd openmpi-3.1.2
-sudo ./configure --prefix=/software/openmpi/3.1.2
+sudo ./configure --prefix=/software
 sudo make
 sudo make all install
 echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/gb773994/.bashrc
