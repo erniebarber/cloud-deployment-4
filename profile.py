1e642f5c7eb0e888a53cabf0cda71e65105ef426
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-10-07 07:56:17 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -85,15 +85,15 @@ for i in range(6):
     node.addService(pg.Execute(shell="sh", command="sudo rm /etc/exports"))
     node.addService(pg.Execute(shell="sh", command="sudo su gb773994 -c 'sudo cp /local/repository/export/export_software /etc/exports'"))
     node.addService(pg.Execute(shell="sh", command="sudo systemctl restart nfs-server"))
-    node.addService(pg.Execute(shell="sh", command="sleep 7m"))
+    node.addService(pg.Execute(shell="sh", command="sleep 2m"))
     node.addService(pg.Execute(shell="sh", command="sudo mount -t nfs 192.168.1.3:/scratch /scratch"))
     
   #mount /scratch and /software on each compute node
   if i > 2:
-    node.addService(pg.Execute(shell="sh", command="sleep 8m"))
+    node.addService(pg.Execute(shell="sh", command="sleep 2m"))
     node.addService(pg.Execute(shell="sh", command="sudo mount -t nfs 192.168.1.3:/scratch /scratch"))
-	#it takes a while for mpi to be installed on the head node so each compute node will pause here for a while until then
-    node.addService(pg.Execute(shell="sh", command="sleep 35m"))
+    #it takes a while for mpi to be installed on the head node so each compute node will pause here for a while until then
+    node.addService(pg.Execute(shell="sh", command="sleep 28m"))
     node.addService(pg.Execute(shell="sh", command="sudo mount -t nfs 192.168.1.1:/software /software"))
     node.addService(pg.Execute(shell="sh", command="sudo chmod 777 /local/repository/scripts/mpi_path_setup.sh"))
     node.addService(pg.Execute(shell="sh", command="sudo -H -u gb773994 bash -c '/local/repository/scripts/mpi_path_setup.sh'"))   
