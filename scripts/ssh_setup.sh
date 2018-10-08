124164af0ce9682dd3bc7ef650f1ee5b28338a1d
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-10-08 00:03:08 -0400

    Update ssh_setup.sh

--- scripts/ssh_setup.sh
+++ scripts/ssh_setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-ssh-keyscan -H 192.168.1.1 >> ~/.ssh/known_hosts
-ssh 192.168.1.1 -f 'sleep 5'
-ssh-keyscan -H 192.168.1.2 >> ~/.ssh/known_hosts
-ssh 192.168.1.2 -f 'sleep 5'
-ssh-keyscan -H 192.168.1.3 >> ~/.ssh/known_hosts
-ssh 192.168.1.3 -f 'sleep 5'
-ssh-keyscan -H 192.168.1.4 >> ~/.ssh/known_hosts
-ssh 192.168.1.4 -f 'sleep 5'
-ssh-keyscan -H 192.168.1.5 >> ~/.ssh/known_hosts
-ssh 192.168.1.5 -f 'sleep 5'
-ssh-keyscan -H 192.168.1.6 >> ~/.ssh/known_hosts
-ssh 192.168.1.6 -f 'sleep 5'
+for y in range (15):
+  addr = "192.168.1." + str(y+1)
+  ssh-keyscan -H addr >> ~/.ssh/known_hosts
+  ssh addr -f 'sleep 5'
+
+#ssh-keyscan -H 192.168.1.2 >> ~/.ssh/known_hosts
+#ssh 192.168.1.2 -f 'sleep 5'
+#ssh-keyscan -H 192.168.1.3 >> ~/.ssh/known_hosts
+#ssh 192.168.1.3 -f 'sleep 5'
+#ssh-keyscan -H 192.168.1.4 >> ~/.ssh/known_hosts
+#ssh 192.168.1.4 -f 'sleep 5'
+#ssh-keyscan -H 192.168.1.5 >> ~/.ssh/known_hosts
+#ssh 192.168.1.5 -f 'sleep 5'
+#ssh-keyscan -H 192.168.1.6 >> ~/.ssh/known_hosts
+#ssh 192.168.1.6 -f 'sleep 5'
