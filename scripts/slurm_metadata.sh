d10c01618a2409772f77a7d71ebc5aad232504ed
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-11-26 11:27:30 -0500

    Update slurm_metadata.sh

--- scripts/slurm_metadata.sh
+++ scripts/slurm_metadata.sh
@@ -1,1 +1,78 @@
+#!/bin/bash
+
+sudo yum install mariadb-server mariadb-devel -y
+
+#create global users
+sudo export MUNGEUSER=991
+sudo groupadd -g $MUNGEUSER munge
+sudo useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge  -s /sbin/nologin munge
+sudo export SLURMUSER=992
+sudo groupadd -g $SLURMUSER slurm
+sudo useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
+
+#install munge
+sudo yum install epel-release -y
+sudo yum install munge munge-libs munge-devel -y
+
+#get secret key from head and pass to etc/munge
+while [ ! -f /scratch/munge.key ]
+do
+  sleep 5
+done
+sudo cp /scratch/munge.key /etc/munge/munge.key
+
+#correct permissions
+sudo chown -R munge: /etc/munge/ /var/log/munge/
+sudo chmod 0700 /etc/munge/ /var/log/munge/
+
+#start munge service
+sudo systemctl enable munge
+sudo systemctl start munge
+
+#install slurm dependencies
+sudo yum install openssl openssl-devel pam-devel numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel man2html libibmad libibumad -y
+
+while [ ! -f /scratch/rpm.fin ]
+do
+  sleep 10
+done
+sudo yum --nogpgcheck localinstall slurm-18.08.3-1.el7.centos.x86_64.rpm slurm-devel-18.08.3-1.el7.centos.x86_64.rpm slurm-munge-18.08.3-1.el7.centos.x86_64.rpm slurm-perlapi-18.08.3-1.el7.centos.x86_64.rpm slurm-plugins-18.08.3-1.el7.centos.x86_64.rpm slurm-sjobexit-18.08.3-1.el7.centos.x86_64.rpm slurm-sjstat-18.08.3-1.el7.centos.x86_64.rpm slurm-torque-18.08.3-1.el7.centos.x86_64.rpm
+
+#metadata configuration
+sudo mkdir /var/spool/slurmdbd
+sudo chown slurm: /var/spool/slurmdbd
+sudo chmod 755 /var/spool/slurmdbd
+sudo touch /var/log/slurm/slurmdbd.log
+sudo chown slurm: /var/log/slurm/slurmdbd.log
+sudo touch /var/run/slurmdbd.pid
+sudo chown slurm: /var/run/slurmdbd.pid
+sudo cp /scratch/innodb.cnf /etc/my.cnf.d/innodb.cnf
+sudo chown slurm: /etc/my.cnf.d/innodb.cnf
+
+#setup mariedb
+sudo systemctl start mariadb 
+sudo systemctl enable mariadb 
+sudo /usr/bin/mysql_secure_installation<<EOF
+y
+secret
+secret
+y
+y
+y
+y
+EOF
+
+sudo mysql "-psecret" < "/scratch/dbd.sql"
+
+#setup clock
+sudo yum install ntp -y
+sudo chkconfig ntpd on
+sudo ntpdate pool.ntp.org
+sudo systemctl start ntpd
+
+#enable slurm daemon
+sudo systemctl enable slurmdbd
+sudo systemctl start slurmdbd
+sudo touch /scratch/dbd.fin
+
 
