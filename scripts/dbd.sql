45da90683493a18a81a36d372d3cf84297d21df6
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-11-26 11:08:18 -0500

    Update dbd.sql

--- scripts/dbd.sql
+++ scripts/dbd.sql
@@ -1,4 +1,4 @@
-grant all on slurm_acct_db.* TO 'slurm'@'localhost' identified by 'some_pass' with grant option;
+grant all on slurm_acct_db.* TO 'slurm'@'localhost' identified by 'secret' with grant option;
 SHOW VARIABLES LIKE 'have_innodb';
 create database slurm_acct_db;
 quit;
