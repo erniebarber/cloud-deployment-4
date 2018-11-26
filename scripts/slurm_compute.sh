d0a93f75df0f85cbcb050ad8408d06630e9561dc
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-11-26 10:30:52 -0500

    Update slurm_compute.sh

--- scripts/slurm_compute.sh
+++ scripts/slurm_compute.sh
@@ -1,1 +1,63 @@
+#!/bin/bash
+
+#create global users
+sudo export MUNGEUSER=991
+sudo groupadd -g $MUNGEUSER munge
+sudo useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge  -s /sbin/nologin munge
+sudo export SLURMUSER=992
+sudo groupadd -g $SLURMUSER slurm
+sudo useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
+
+#install munge
+sudo yum install epel-release -y
+sudo yum install munge munge-libs munge-devel -y
+
+#wait until head creates secret key and pass to etc/munge
+while [ ! -f /scratch/munge.key ]
+do
+  sleep 5
+done
+sudo cp /scratch/munge.key /etc/munge/munge.key
+
+#correct permissions
+sudo chown -R munge: /etc/munge/ /var/log/munge/
+sudo chmod 0700 /etc/munge/ /var/log/munge/
+
+#start munge service
+sudo systemctl enable munge
+sudo systemctl start munge
+
+#install slurm dependencies
+sudo yum install openssl openssl-devel pam-devel numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel man2html libibmad libibumad -y
+
+#install slurm
+while [ ! -f /scratch/rpm.key ]
+do
+  sleep 10
+done
+sudo yum --nogpgcheck localinstall slurm-18.08.3-1.el7.centos.x86_64.rpm slurm-devel-18.08.3-1.el7.centos.x86_64.rpm slurm-munge-18.08.3-1.el7.centos.x86_64.rpm slurm-perlapi-18.08.3-1.el7.centos.x86_64.rpm slurm-plugins-18.08.3-1.el7.centos.x86_64.rpm slurm-sjobexit-18.08.3-1.el7.centos.x86_64.rpm slurm-sjstat-18.08.3-1.el7.centos.x86_64.rpm slurm-torque-18.08.3-1.el7.centos.x86_64.rpm
+
+#compute configuration
+sudo mkdir /var/spool/slurmd
+sudo chown slurm: /var/spool/slurmd
+sudo chmod 755 /var/spool/slurmd
+sudo touch /var/log/slurmd.log
+sudo chown slurm: /var/log/slurmd.log
+
+#setup clock
+sudo yum install ntp -y
+sudo chkconfig ntpd on
+sudo ntpdate pool.ntp.org
+sudo systemctl start ntpd
+
+#enable slurm daemon
+while [ ! -f /scratch/dbd.fin ]
+do
+  sleep 5
+done
+sudo systemctl enable slurmd
+sudo systemctl start slurmd
+sudo rm /scratch/dbd.fin
+sudo touch /scratch/d.fin
+
 
