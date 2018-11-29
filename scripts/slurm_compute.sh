61331c0c375eea1fd5d72904fd4d777f3665e89d
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-11-29 14:39:08 -0500

    Update slurm_compute.sh

--- scripts/slurm_compute.sh
+++ scripts/slurm_compute.sh
@@ -40,6 +40,8 @@ while [ ! -f /scratch/rpm.fin ]
 do
   sleep 10
 done
+
+cd /software/slurm-rpms
 sudo yum --nogpgcheck localinstall slurm-18.08.3-1.el7.centos.x86_64.rpm slurm-devel-18.08.3-1.el7.centos.x86_64.rpm slurm-munge-18.08.3-1.el7.centos.x86_64.rpm slurm-perlapi-18.08.3-1.el7.centos.x86_64.rpm slurm-plugins-18.08.3-1.el7.centos.x86_64.rpm slurm-sjobexit-18.08.3-1.el7.centos.x86_64.rpm slurm-sjstat-18.08.3-1.el7.centos.x86_64.rpm slurm-torque-18.08.3-1.el7.centos.x86_64.rpm
 
 #compute configuration
