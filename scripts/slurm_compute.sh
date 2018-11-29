5a62e7a448bf6eb8d6d860543ea41011dabb3f2e
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-11-29 10:08:21 -0500

    Update slurm_compute.sh

--- scripts/slurm_compute.sh
+++ scripts/slurm_compute.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #create global users
-sudo export MUNGEUSER=991
+export MUNGEUSER=991
 sudo groupadd -g $MUNGEUSER munge
 sudo useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge  -s /sbin/nologin munge
-sudo export SLURMUSER=992
+export SLURMUSER=992
 sudo groupadd -g $SLURMUSER slurm
 sudo useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
 
@@ -15,13 +15,18 @@ sudo yum install munge munge-libs munge-devel -y
 #wait until head creates secret key and pass to etc/munge
 while [ ! -f /scratch/munge.key ]
 do
-  sleep 5
+  sleep 10
 done
 sudo cp /scratch/munge.key /etc/munge/munge.key
 
 #correct permissions
 sudo chown -R munge: /etc/munge/ /var/log/munge/
 sudo chmod 0700 /etc/munge/ /var/log/munge/
+
+while [ ! -f /scratch/metakey.fin ]
+do
+  sleep 10
+done
 
 #start munge service
 sudo systemctl enable munge
