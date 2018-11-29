8d53035ba4774ccfa6e9054e7fb1ec6c9cc10d4e
GJBarretta <30812427+GJBarretta@users.noreply.github.com>
2018-11-29 09:56:53 -0500

    Update slurm_head.sh

--- scripts/slurm_head.sh
+++ scripts/slurm_head.sh
@@ -4,7 +4,7 @@
 export MUNGEUSER=991
 sudo groupadd -g $MUNGEUSER munge
 sudo useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge  -s /sbin/nologin munge
-sudo export SLURMUSER=992
+export SLURMUSER=992
 sudo groupadd -g $SLURMUSER slurm
 sudo useradd  -m -c "SLURM workload manager" -d /var/lib/slurm -u $SLURMUSER -g slurm  -s /bin/bash slurm
 
@@ -14,20 +14,19 @@ sudo yum install munge munge-libs munge-devel -y
 
 #munge secret key setup
 sudo yum install rng-tools -y
-sudo rngd -r /dev/urandom
+sudo rngd -r /dev/urandom --no-tpm=1
 
 #create munge secret key
 sudo /usr/sbin/create-munge-key -r
-sudo dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key
 sudo chown munge: /etc/munge/munge.key
 sudo chmod 400 /etc/munge/munge.key
 
 #pass munge secret key to compute nodes
-sudo cp /etc/munge/munge.key /scratch
+sudo cp /etc/munge/munge.key /scratch/munge.key
 
 #correct permissions
 sudo chown -R munge: /etc/munge/ /var/log/munge/
-sudo chmod 0700 /etc/munge/ /var/log/munge/
+chmod 0700 /etc/munge/ /var/log/munge/
 
 #start munge service
 sudo systemctl enable munge
