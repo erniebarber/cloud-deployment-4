c03e79bd2dbe668aa00abcd19be89317412fd1d2
Giuseppe Barretta <30812427+GJBarretta@users.noreply.github.com>
2018-12-02 17:47:04 -0500

    Update sbatch.sh

--- scripts/sbatch.sh
+++ scripts/sbatch.sh
@@ -1,17 +1,22 @@
-#!/bin/bash
-#SBATCH -t 1:00:00
-#SBATCH -N 12
-#SBATCH --ntasks-per-node 1
-
-#echo command to stdout
-set -x
-echo $SLURM_SUBMIT_DIR
-module load /software/mpi/gcc_open
-
-cd /scratch
-mpicc -lm /scratch/pi_mc.c
-
-for i in {2..12..2} do 
-  echo "With ${i} processes"
-  time mpirun -np $i ./a.out
-done
+#!/bin/bash 
+#SBATCH -t 1:00:00 
+#SBATCH -N 12 
+#SBATCH --ntasks-per-node 1 
+#echo command to stdout 
+set -x 
+echo $SLURM_SUBMIT_DIR 
+module load /software/mpi/gcc_open 
+cd /scratch 
+mpicc -lm /scratch/pi_mc.c 
+echo "With 2 processes" 
+time mpirun -np 2 --mca btl_base_warn_component_unused 0 /scratch/a.out 100000000 
+echo "With 4 processes" 
+time mpirun -np 4 --mca btl_base_warn_component_unused 0 /scratch/a.out 100000000 
+echo "With 6 processes" 
+time mpirun -np 6 --mca btl_base_warn_component_unused 0 /scratch/a.out 100000000 
+echo "With 8 processes" 
+time mpirun -np 8 --mca btl_base_warn_component_unused 0 /scratch/a.out 100000000 
+echo "With 10 processes" 
+time mpirun -np 10 --mca btl_base_warn_component_unused 0 /scratch/a.out 100000000 
+echo "With 12 processes" 
+time mpirun -np 12 --mca btl_base_warn_component_unused 0 /scratch/a.out 100000000
