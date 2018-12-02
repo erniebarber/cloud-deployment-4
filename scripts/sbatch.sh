cd42a63e341ce78c7a505d4d2d873efc7ad9bf42
Giuseppe Barretta <30812427+GJBarretta@users.noreply.github.com>
2018-12-02 09:43:51 -0500

    Update sbatch.sh

--- scripts/sbatch.sh
+++ scripts/sbatch.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 #SBATCH -p RM
 #SBATCH -t 1:00:00
-#SBATCH -N 1
-#SBATCH --ntasks-per-node 28
+#SBATCH -N 3
+#SBATCH --ntasks-per-node 1
 
 #echo command to stdout
 set -x
 echo $SLURM_SUBMIT_DIR
 module load /software/mpi/gcc_open
-mpi
 
-cd /SCRATCH
+cd /scratch
 mpicc /scratch/hello.c
 
 #for i in {2..28..2} 
 #do 
 # echo "With ${i} processes"
- time mpirun -np $i ./a.out
+ time mpirun -np 3 ./a.out
 #done
