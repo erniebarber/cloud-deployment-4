b6ba370c0136a06068258a33c4898d65ec750af0
Ernie Barber <erniebarber@me.com>
2018-12-13 11:40:14 -0500

    Update mpi_path_setup.sh

--- scripts/mpi_path_setup.sh
+++ scripts/mpi_path_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "export PATH='$PATH:/software/bin'" >> /users/gb773994/.bashrc
-echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/gb773994/.bashrc
-source /users/gb773994/.bashrc
+echo "export PATH='$PATH:/software/bin'" >> /users/eb881047/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/eb881047/.bashrc
+source /users/eb881047/.bashrc
